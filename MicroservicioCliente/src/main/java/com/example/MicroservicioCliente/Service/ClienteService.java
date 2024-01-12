0424fb83795703d21ec4b753d8da477615d97e9b
Javicorporation <krchipullajavier@gmail.com>
2024-01-12 00:53:13 -0500

    siii

--- MicroservicioCliente/src/main/java/com/example/MicroservicioCliente/Service/ClienteService.java
+++ MicroservicioCliente/src/main/java/com/example/MicroservicioCliente/Service/ClienteService.java
@@ -1,30 +1,6 @@
 package com.example.MicroservicioCliente.Service;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.example.MicroservicioCliente.Entity.Cliente;
-import com.example.MicroservicioCliente.Repository.ClienteRepository;
-
-@Service
 public class ClienteService {
-    @Autowired
-    ClienteRepository clienteRepository;
-
-    public List<Cliente> getAll(){
-        return clienteRepository.findAll();
-    }
-
-    public Optional getClienteById(Long id){
-        return clienteRepository.findById(id);
-    }
-
-    public void saveCliente(Cliente cliente){
-        clienteRepository.save(cliente);
-        
-    }
+    
 
 }
