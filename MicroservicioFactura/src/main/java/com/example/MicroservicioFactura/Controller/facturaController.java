eab73e8cbe74d7475785a243968c572bc8a33876
Javicorporation <krchipullajavier@gmail.com>
2024-01-12 12:12:00 -0500

    listo

--- MicroservicioFactura/src/main/java/com/example/MicroservicioFactura/Controller/facturaController.java
+++ MicroservicioFactura/src/main/java/com/example/MicroservicioFactura/Controller/facturaController.java
@@ -1,9 +1,12 @@
 package com.example.MicroservicioFactura.Controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.MicroservicioFactura.Entity.Factura;
+import com.example.MicroservicioFactura.Entity.Validation.Error;
 import com.example.MicroservicioFactura.Service.FacturaService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/apiFactura")
@@ -40,7 +46,15 @@ public class facturaController {
 
 
     @PostMapping()
-    public ResponseEntity <Factura> guardar (@RequestBody Factura factura){
+    public ResponseEntity <?> guardar (@Valid @RequestBody Factura factura, BindingResult result){
+        if(result.hasErrors()){
+            List<String> messages = result.getAllErrors().stream()
+                .map(ObjectError::getDefaultMessage)
+                .collect(Collectors.toList());
+            String errorMessage = String.join(", ", messages);
+            return ResponseEntity.badRequest().body(new Error("validation Error", errorMessage));
+
+        }
         Factura newFactura = facturaService.save(factura);
         return ResponseEntity.ok(newFactura);
     }
--- MicroservicioFactura/src/main/java/com/example/MicroservicioFactura/Entity/Factura.java
+++ MicroservicioFactura/src/main/java/com/example/MicroservicioFactura/Entity/Factura.java
@@ -8,6 +8,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -44,12 +46,23 @@ public class Factura {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "fecha de expedicion", nullable = false)
+
+    
+    @Column(name = "Fecha de expedicion", nullable = false)
     private Date fechaExp;
+
+    @NotBlank(message = "El tipo de factura no puede ser vacio")
+    @NotNull(message = "El tipo de factura no puede ser nulo ")
     @Column(name = "tipo", nullable = false)
     private String type;
+
+    @NotBlank(message = "El nombre de la tienda no puede ser vacio")
+    @NotNull(message = "El nombre de la tienda de factura no puede ser nulo ")
     @Column(name = "nombre de tienda", nullable = false)
     private String storeName;
+
+    @NotBlank(message = "El dato vendedor de factura no puede ser vacio")
+    @NotNull(message = "El dato vendedor de factura no puede ser nulo ")
     @Column(name = "vendedor", nullable = false)
     private String sellerName;
 
